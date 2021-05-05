4a5e20b0a966337d37bba6f1094b7e65ee739f0e
Cole Nockolds <cole.nockolds@utexas.edu>
2021-05-05 00:42:12 -0500

    final

--- finalProject/source/api.py
+++ finalProject/source/api.py
@@ -3,6 +3,7 @@ import redis
 from flask import Flask, request
 import os
 import jobs
+import datetime
 
 app = Flask(__name__)
 
@@ -15,12 +16,48 @@ rd=redis.StrictRedis(host=redis_ip, port=6379, db=0)
 def populate_redis():
     rd.flushdb()
     restaurants = getdata()
+    #return restaurants['Restaurants'][(i)]
     for i in range(len(restaurants['Restaurants'])):
+        print(restaurants['Restaurants'][(i)])
+    #return "done"  
         key="key"+str(i+1)
         rd.hmset(key,restaurants['Restaurants'][(i)])
-        key = ""
-    rd.delete('Restaurants')
-    return key+"\n"
+        #key = ""
+    #rd.delete('Restaurants')
+    return "Redis Data Reset"+"\n"
+
+@app.route('/create/<key>/<name>/<address>/<zipcode>/<date>/<score>', methods=['GET'])
+def create(key, name, address, zipcode, date, score):
+    rd.hmset(key, {'Restaurant Name': name, 'Address': address, 'Zip Code': zipcode, 'Date of Inspection': date, 'Inspection Score': score})
+    return "New Restaurant: "+str(rd.hgetall(key))+"\n"
+
+@app.route('/read/<key>', methods=['GET'])
+def select(key):
+    return str(rd.hgetall(key))+"\n"
+
+@app.route('/update/<key>/<datapoint>/<new>', methods=['GET'])
+def edit_restaurant(key, datapoint, new):
+    if datapoint == 'restaurantname':
+        rd.hset(key, 'Restaurant Name', new)
+    elif datapoint == 'zipcode':
+        rd.hset(key, 'Zip Code', new)
+    elif datapoint == 'date':
+        rd.hset(key, 'Date of Inspection', new)
+    elif datapoint == 'score':
+        rd.hset(key, 'Inspection Score', new)
+    else:
+        rd.hset(key, datapoint, new)
+    return "Updated Restaurant: "+str(rd.hgetall(key))+"\n"
+
+@app.route('/delete/<key>', methods=['GET'])
+def delete(key):
+    rd.delete(key)
+    return "Entry deleted."+"\n"
+
+@app.route('/emptydb', methods=['GET'])
+def emptydb()
+    rd.flushdb()
+    return "Database emptied."+"\n"
 
 @app.route('/jobs', methods=['POST'])
 def jobs_api():
